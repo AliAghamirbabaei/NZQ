ce564f1d3fa2404f01bedeb9b878ff1b81cb7ea4
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-11-24 16:03:59 +0330

    Added model for transaction chart

---
+++ app/src/main/java/NZQ/Model/Chart/BalanceModel.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+//package NZQ.Model.Chart;
+//
+//import Model.Date;
+//import Model.Transaction.TransactionType;
+//
+///**
+// *
+// * @author aliaghamirbabaei
+// */
+//public class BalanceModel {
+//    private int price;
+//    private Date date;
+//    private TransactionType transactionType;
+//    
+//    public BalanceModel(int price, Date date, TransactionType transactionType) {
+//        this.price = price;
+//        this.date = date;
+//        this.transactionType = transactionType;
+//    }
+//}
