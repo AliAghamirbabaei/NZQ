0100fe4e974ecb4adf457ffe280cf6d3a120a10d
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-11-24 16:03:38 +0330

    Comment useless methods

--- app/src/main/java/NZQ/ViewModel/Report/DebtCreditReport.java
+++ app/src/main/java/NZQ/ViewModel/Report/DebtCreditReport.java
@@ -4,8 +4,6 @@ import Model.Date;
 import Model.Transaction.Paid;
 import Model.Transaction.PrePaid;
 import Model.Transaction.PrePaidStatus;
-import Model.Transaction.Transaction;
-import Model.Transaction.TransactionType;
 
 import java.util.ArrayList;
 
@@ -18,94 +16,108 @@ public class DebtCreditReport {
         this.paids = paids;
         this.prePaids = prePaids;
     }
-    
-    public ArrayList<Transaction> generateDateRangeDebt(int fromYear, int toYear, int fromMonth, int toMonth, int fromDay, int toDay) {
-        ArrayList<Transaction> result = new ArrayList<>();
-        
-        for (Paid paid : paids) {
-            //if (paid.getTime().) {
-        
-            //}
-        }
-            
-        return result;
-    }
 
-    public ArrayList<Integer> generateDailyDebt() {
-        ArrayList<Integer> result = new ArrayList<>();
-        Date currentDate = Date.getCurrentDate();
-        for (Paid paid : paids) {
-            if (paid.getTime().getYear() == currentDate.getYear()
-                    && paid.getTime().getMonth() == currentDate.getMonth()
-                    && paid.getTime().getDay() == currentDate.getDay()
-                    && paid.getTransactionType() == TransactionType.DEBT) {
-                result.add(paid.getPrice());
-            }
-        }
-        return result;
-    }
+//    public ArrayList<BalanceModel> generatePaidDateRangeDebt(Date fromDate, Date toDate) {
+//        ArrayList<BalanceModel> result = new ArrayList<>();
+//
+//        int sumDebt = 0;
+//
+//        for (Paid paid : paids) {
+//            if (paid.getTime().getYear() >= fromDate.getYear()
+//                    && paid.getTime().getYear() <= toDate.getYear()
+//                    && paid.getTime().getMonth() >= fromDate.getMonth()
+//                    && paid.getTime().getMonth() <= toDate.getMonth()
+//                    && paid.getTime().getDay() >= fromDate.getDay()
+//                    && paid.getTime().getDay() <= toDate.getDay()) {
+//                if (paid.getTime().getDay() == result.lastIndexOf(paid)) {
+//                    if (paid.getTransactionType() == TransactionType.DEBT) {
+//                        sumDebt += paid.getPrice();
+//                    }
+//                } else {
+//                    currentDay = paid.getTime().getDay();
+//                    result.add(new BalanceModel(sumDebt, paid.getTime(), TransactionType.DEBT));
+//                }
+//            }
+//        }
+//
+//        return result;
+//    }
 
-    public ArrayList<Integer> generateMonthlyDebt(int year, int month) {
-        ArrayList<Integer> result = new ArrayList<>();
-        for (Paid paid : paids) {
-            if (paid.getTime().getYear() == year
-                    && paid.getTime().getMonth() == month
-                    && paid.getTransactionType() == TransactionType.DEBT) {
-                result.add(paid.getPrice());
-            }
-        }
-        return result;
-    }
-
-    public ArrayList<Integer> generateYearlyDebt(int year) {
-        ArrayList<Integer> result = new ArrayList<>();
-        for (Paid paid : paids) {
-            System.out.println(paid.getTime().getYear());
-            if (paid.getTime().getYear() == year
-                    && paid.getTransactionType() == TransactionType.DEBT) {
-                result.add(paid.getPrice());
-            }
-        }
-        return result;
-    }
-
-    public ArrayList<Integer> generateDailyCredit() {
-        ArrayList<Integer> result = new ArrayList<>();
-        Date currentDate = Date.getCurrentDate();
-        for (PrePaid prePaid : prePaids) {
-            if (prePaid.getTime().getYear() == currentDate.getYear()
-                    && prePaid.getTime().getMonth() == currentDate.getMonth()
-                    && prePaid.getTime().getDay() == currentDate.getDay()
-                    && prePaid.getTransactionType() == TransactionType.CREDIT) {
-                result.add(prePaid.getPrice());
-            }
-        }
-        return result;
-    }
-
-    public ArrayList<Integer> generateMonthlyCredit(int year, int month) {
-        ArrayList<Integer> result = new ArrayList<>();
-        for (PrePaid prePaid : prePaids) {
-            if (prePaid.getTime().getYear() == year
-                    && prePaid.getTime().getMonth() == month
-                    && prePaid.getTransactionType() == TransactionType.CREDIT) {
-                result.add(prePaid.getPrice());
-            }
-        }
-        return result;
-    }
-
-    public ArrayList<Integer> generateYearlyCredit(int year) {
-        ArrayList<Integer> result = new ArrayList<>();
-        for (PrePaid prePaid : prePaids) {
-            if (prePaid.getTime().getYear() == year
-                    && prePaid.getTransactionType() == TransactionType.CREDIT) {
-                result.add(prePaid.getPrice());
-            }
-        }
-        return result;
-    }
-
+//    public ArrayList<Integer> generateDailyDebt() {
+//        ArrayList<Integer> result = new ArrayList<>();
+//        Date currentDate = Date.getCurrentDate();
+//        for (Paid paid : paids) {
+//            if (paid.getTime().getYear() == currentDate.getYear()
+//                    && paid.getTime().getMonth() == currentDate.getMonth()
+//                    && paid.getTime().getDay() == currentDate.getDay()
+//                    && paid.getTransactionType() == TransactionType.DEBT) {
+//                result.add(paid.getPrice());
+//            }
+//        }
+//        return result;
+//    }
+//
+//    public ArrayList<Integer> generateMonthlyDebt(int year, int month) {
+//        ArrayList<Integer> result = new ArrayList<>();
+//        for (Paid paid : paids) {
+//            if (paid.getTime().getYear() == year
+//                    && paid.getTime().getMonth() == month
+//                    && paid.getTransactionType() == TransactionType.DEBT) {
+//                result.add(paid.getPrice());
+//            }
+//        }
+//        return result;
+//    }
+//
+//    public ArrayList<Integer> generateYearlyDebt(int year) {
+//        ArrayList<Integer> result = new ArrayList<>();
+//        for (Paid paid : paids) {
+//            System.out.println(paid.getTime().getYear());
+//            if (paid.getTime().getYear() == year
+//                    && paid.getTransactionType() == TransactionType.DEBT) {
+//                result.add(paid.getPrice());
+//            }
+//        }
+//        return result;
+//    }
+//
+//    public ArrayList<Integer> generateDailyCredit() {
+//        ArrayList<Integer> result = new ArrayList<>();
+//        Date currentDate = Date.getCurrentDate();
+//        for (PrePaid prePaid : prePaids) {
+//            if (prePaid.getTime().getYear() == currentDate.getYear()
+//                    && prePaid.getTime().getMonth() == currentDate.getMonth()
+//                    && prePaid.getTime().getDay() == currentDate.getDay()
+//                    && prePaid.getTransactionType() == TransactionType.CREDIT) {
+//                result.add(prePaid.getPrice());
+//            }
+//        }
+//        return result;
+//    }
+//
+//    public ArrayList<Integer> generateMonthlyCredit(int year, int month) {
+//        ArrayList<Integer> result = new ArrayList<>();
+//        for (PrePaid prePaid : prePaids) {
+//            if (prePaid.getTime().getYear() == year
+//                    && prePaid.getTime().getMonth() == month
+//                    && prePaid.getTransactionType() == TransactionType.CREDIT) {
+//                result.add(prePaid.getPrice());
+//            }
+//        }
+//        return result;
+//    }
+//
+//    public ArrayList<Integer> generateYearlyCredit(int year) {
+//        ArrayList<Integer> result = new ArrayList<>();
+//        for (PrePaid prePaid : prePaids) {
+//            if (prePaid.getTime().getYear() == year
+//                    && prePaid.getTransactionType() == TransactionType.CREDIT) {
+//                result.add(prePaid.getPrice());
+//            }
+//        }
+//        return result;
+//    }
+//
     public int getVertex() {
         if (!prePaids.isEmpty()) {
             int sum = 0;
